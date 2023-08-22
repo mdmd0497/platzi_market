3cf4e693fb7ca4cc7d6232acc140eea5a739dba1
mdmd0497 <mdmd0497@hotmail.com>
2023-08-21 20:41:45 -0500

    se hace cambios para poder usar JPA o spring data

---
+++ src/main/java/com/platzimarket/PlatziMarketApplication.java
@@ -0,0 +1,16 @@
+package com.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude= DataSourceAutoConfiguration.class)
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
