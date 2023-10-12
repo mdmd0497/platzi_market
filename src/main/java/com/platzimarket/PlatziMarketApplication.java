6b8b5377969a30f4ce307fb7ae574c0f07f9ca80
mdmd0497 <mdmd0497@hotmail.com>
2023-10-12 11:55:37 -0500

    cambios para dejar versiones en java correctas

--- src/main/java/com/platzimarket/PlatziMarketApplication.java
+++ src/main/java/com/platzimarket/PlatziMarketApplication.java
@@ -1,16 +1,11 @@
 package com.platzimarket;
 
 import org.springframework.boot.SpringApplication;
-<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableAutoConfiguration(exclude= DataSourceAutoConfiguration.class)
-=======
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
->>>>>>> 20995954c86de208fa6b0b0af3bbdb40b5fcbf5e
 @SpringBootApplication
 public class PlatziMarketApplication {
 
