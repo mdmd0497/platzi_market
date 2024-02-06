98590968634d1dc943f21097c7c6c2cd96395cb7
mdmd0497 <mdmd0497@hotmail.com>
2024-02-06 14:22:24 -0500

    se añade uso de los query methods hace falta usar mas querymethods

--- src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,14 @@
 package com.platzimarket.persistence.crud;
 
 import com.platzimarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+//    @Query(value = "SELECT * FROM productos where id_categoria = ?",nativeQuery = true)
+    List <Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
 }
